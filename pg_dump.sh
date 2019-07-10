4650e5f5ff4379f6f94c4a10770e37c47feb9553
tuanld <tuan.ld@isofh.com>
2019-07-10 15:39:21 +0700

    update backup database

--- pg_dump.sh
+++ pg_dump.sh
@@ -2,5 +2,5 @@ CONTAINER=$1
 DATABASE=$2
 echo "$(date) starting backup container $CONTAINER database $DATABASE"
 docker exec -t $CONTAINER mkdir -p /backup/$DATABASE
-docker exec -t $CONTAINER pg_dump -U postgres --no-owner -Fc $DATABASE -f /backup/$DATABASE/dump_`date +%d-%m-%Y"_"%H_%M_%S`.dmp 
+docker exec -t $CONTAINER pg_dump -U postgres --no-owner -Fc $DATABASE -f /backup/$DATABASE/dump_$(date +%d-%m-%Y"_"%H_%M_%S).dmp 
 echo "$(date) finish backup container $CONTAINER database $DATABASE"
